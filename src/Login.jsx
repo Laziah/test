6ab4643e59ce9164acffa154eeadb2b98bcaf275
Laziah <Laziah2001@gmail.com>
2024-03-01 11:02:55 +0300

    testing the look

--- src/Login.jsx
+++ src/Login.jsx
@@ -26,21 +26,42 @@ function Login() {
 
     return (
         <div className="container">
+
+        <div className="card">
             <div className="row" id="form-height">
-                <div className="col-sm-8 margin" id="back-img">
-                    <p id="para-0"><span id="smart">STREAMLINE BackOffice Analytics Admin Platform </span> Facilitates seamless back-office management, planning, and scheduling, ensuring the delivery of uncompromising quality, right on time.</p>
+                <div className="col-sm-7 margin" id="back-img">
+                    <p id="para-0">
+                    <span id="smart" style={{ color: 'white' }}>STREAMLINE BACK OFFICE</span><br />
+                        Facilitates seamless back-office management, planning,<br /> and scheduling,
+                        ensuring the delivery of uncompromising <br /> quality, right on time.
+                    </p>
                 </div>
                 <div className="col-sm-4 margin">
                     <h2>Login</h2>
                     <form name="signup_form" id="signup_form" onSubmit={handleSubmit}>
                         <div className="form-group">
                             <label htmlFor="email">Email address<span className="red">*</span></label>
-                            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" onChange={(e) => setEmail(e.target.value)} />
-                            <span className="error" id="emailerror">{error && error}</span> {/* Display error message */}
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="email"
+                                aria-describedby="emailHelp"
+                                placeholder="Enter email"
+                                name="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <span className="error" id="emailerror">{error && error}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password<span className="red">*</span></label>
-                            <input type="password" className="form-control" id="password" placeholder="Password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                placeholder="Password"
+                                name="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                             <span className="error" id="passworderror"></span>
                         </div>
                         <button type="submit" className="btn btn-outline-warning form-width" id="submit" name="submit">Login</button>
@@ -49,6 +70,7 @@ function Login() {
                 </div>
             </div>
         </div>
+    </div>
     )
 }
 
